47e02da1f0c9931cdc4788d460f55ce120f62685
Aziz <79049457+Eternal-uz@users.noreply.github.com>
2022-11-08 12:37:26 +0500

    first commit

---
+++ Redux-Shopping-Cart-App/src/App.js
@@ -0,0 +1,41 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./App.css";
+import Auth from "./components/Auth";
+import Layout from "./components/Layout";
+import Notification from "./components/Notification";
+import { fetchData, sendCartData } from "./store/cart-actions";
+let firstRender = true;
+
+function App() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (firstRender) {
+      firstRender = false;
+      return;
+    }
+    if (cart.changed) {
+      dispatch(sendCartData(cart));
+    }
+  }, [cart, dispatch]);
+
+  return (
+    <div className="App">
+      {notification && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
+      {!isLoggedIn && <Auth />}
+      {isLoggedIn && <Layout />}
+    </div>
+  );
+}
+
+export default App;
---
+++ Redux-Shopping-Cart-App/src/store/cart-actions.js
@@ -0,0 +1,69 @@
+import { cartActions } from "./cart-slice";
+import { uiActions } from "./ui-slice";
+
+export const fetchData = () => {
+  return async (dispatch) => {
+    const fetchHandler = async () => {
+      const res = await fetch(
+        "https://redux-3b12c-default-rtdb.firebaseio.com/cartItems.json"
+      );
+      const data = await res.json();
+      console.log(data);
+      return data;
+    };
+    try {
+      const cartData = await fetchHandler();
+      dispatch(cartActions.replaceData(cartData));
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Request failed",
+          type: "error",
+        })
+      );
+    }
+  };
+};
+
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        open: true,
+        message: "Sending request",
+        type: "warning",
+      })
+    );
+    const sendRequest = async () => {
+      const res = await fetch(
+        "https://redux-3b12c-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Request sent",
+          type: "success",
+        })
+      );
+    };
+
+    try {
+      await sendRequest();
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Request failed",
+          type: "error",
+        })
+      );
+    }
+  };
+};
